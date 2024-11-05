5232eb0d95147ea9ac0c962c1edda3527af1eb4e
Danil <dd.wasabully@gmail.com>
2024-11-05 15:25:53 +0300

    feat: add player functionality, styles, and new tracks

--- js/script.js
+++ js/script.js
@@ -5,13 +5,28 @@ const nextButton = document.getElementById('next-button');
 const albumCover = document.getElementById('album-cover');
 const trackTitle = document.getElementById('track-title');
 const trackArtist = document.getElementById('track-artist');
+const progressBar = document.querySelector('.progress-bar');
+const progressTimeStart = document.querySelector('.start-time');
+const progressTimeEnd = document.querySelector('.end-time');
 
 const tracks = [
+	{
+		title: 'Beautiful Day',
+		artist: 'Akon',
+		src: "/music/Akon - Akon's Beautiful Day.mp3",
+		img: '/img/img-AKON.jpeg',
+	},
 	{
 		title: 'Love The Way You Lie',
 		artist: 'Eminem - Rihanna',
 		src: '/music/Eminem, Rihanna – Love The Way You Lie.mp3',
 		img: '/img/img-LoveTheWayYouLie.jpeg',
+	},
+	{
+		title: 'Get Lucky',
+		artist: 'Daft Punk',
+		src: '/music/Daft Punk - Get Lucky.mp3',
+		img: '/img/img-Get-Lucky.jpg',
 	},
 	{
 		title: "Gangsta's Paradise",
@@ -24,12 +39,23 @@ const tracks = [
 let currentTrackIndex = 0;
 
 const loadTrack = () => {
-	const track = tracks[currentTrackIndex];
-	trackTitle.textContent = track.title;
-	trackArtist.textContent = track.artist;
-	albumCover.src = track.img;
-	audio.querySelector('source').src = track.src;
+	const { title, artist, src, img } = tracks[currentTrackIndex];
+	trackTitle.textContent = title;
+	trackArtist.textContent = artist;
+	albumCover.src = img;
+	audio.querySelector('source').src = src;
+
+	const background = document.getElementById('background');
+	background.style.backgroundImage = `url(${img})`;
+
 	audio.load();
+	progressBar.value = 0;
+};
+
+const formatTime = (time) => {
+	const minutes = Math.floor(time / 60);
+	const seconds = Math.floor(time % 60);
+	return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 };
 
 const playPauseTrack = () => {
@@ -56,8 +82,32 @@ const prevTrack = () => {
 	playButton.innerHTML = '&#10074;&#10074;';
 };
 
+audio.addEventListener('loadedmetadata', () => {
+	progressTimeEnd.textContent = formatTime(audio.duration);
+});
+
+audio.addEventListener('timeupdate', () => {
+	if (audio.duration) {
+		progressBar.value = (audio.currentTime / audio.duration) * 100;
+		progressTimeStart.textContent = formatTime(audio.currentTime);
+	}
+});
+
+progressBar.addEventListener('input', () => {
+	const newTime = (progressBar.value / 100) * audio.duration;
+	audio.currentTime = newTime;
+});
+
 playButton.addEventListener('click', playPauseTrack);
 nextButton.addEventListener('click', nextTrack);
 prevButton.addEventListener('click', prevTrack);
+
+audio.addEventListener('error', () => {
+	console.error('Ошибка при загрузке аудиофайла: ', audio.error);
+});
+
+audio.addEventListener('ended', () => {
+	nextTrack();
+});
 
 loadTrack();
