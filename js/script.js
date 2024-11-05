eddbd0c13c379c8ed16f6caaae7dd3129511be17
Danil <dd.wasabully@gmail.com>
2024-11-05 10:30:37 +0300

    feat: Added a new track

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 const audio = document.querySelector('.player__audio');
 const playButton = document.getElementById('play-button');
 
+const tracks = [
+	{
+		title: 'Love The Way You Lie',
+		artist: 'Eminem - Rihanna',
+		src: '/music/Eminem, Rihanna – Love The Way You Lie.mp3',
+		img: '/img/img-LoveTheWayYouLie.jpeg',
+	},
+	{
+		title: "Gangsta's Paradise",
+		artist: 'Coolio (feat. L.V.)',
+		src: "/music/Coolio feat LV - Gangsta's Paradise.mp3",
+		img: '/img/img-Paradise.jpg',
+	},
+];
+
 function playPauseTrack() {
 	if (audio.paused) {
 		audio.play();
